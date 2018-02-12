0abc63ab16cc09ab8004493ea714b6e01d2425f2
Galen Han <Mastergalen@users.noreply.github.com>
2018-02-12 00:18:13 +0000

    Load video based on ?video query parameter

--- app/src/app.js
+++ app/src/app.js
@@ -1,8 +1,30 @@
 import 'bootstrap';
+import $ from 'jquery';
 import { Map } from 'immutable';
 import './scss/main.scss';
 import { startDrawingOverlay } from './lib/overlay';
 import './lib/annotations-table';
+
+/**
+ * Get the query string parameter
+ * @param {string} key
+ */
+function qs(key) {
+  const escapedKey = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+  const match = location.search.match(new RegExp("[?&]"+escapedKey+"=([^&]+)(&|$)"));
+  return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+}
+
+const youtubeId = qs('video');
+
+if (youtubeId === null) {
+  console.error('?video= parameter not set');
+  alert('Video parameter not set');
+}
+
+const videoDir = `videos/${youtubeId}`;
+
+$('#video source').attr('src', `${videoDir}.mp4`);
 
 const players = plyr.setup();
 global.annotationMap = Map();
@@ -22,7 +44,8 @@ document.body.onkeyup = (e) => {
 
 // TODO Do not enable play button until videoData is loaded
 
-fetch('videos/-3K26M-m_00.json').then(res => res.json()).then((json) => {
+fetch(`${videoDir}.json`).then(res => res.json()).then((json) => {
   const videoData = json;
   startDrawingOverlay(videoData);
 });
+
