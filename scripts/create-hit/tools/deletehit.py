d74b8af3454f8083dbebea87e70f03597df51546
Galen Han <Mastergalen@users.noreply.github.com>
2018-02-23 19:26:39 +0000

    Add MTurk scripts for debugging

---
+++ scripts/create-hit/tools/deletehit.py
@@ -0,0 +1,18 @@
+import argparse
+import pprint
+from lib import apiclient
+
+parser = argparse.ArgumentParser()
+parser.add_argument('hitid', type=str, help='HIT ID')
+args = parser.parse_args()
+
+ENDPOINT = 'https://mturk-requester.us-east-1.amazonaws.com'
+
+client = apiclient.create(ENDPOINT)
+
+print("Fetching HIT {}".format(args.hitid))
+
+response = client.delete_hit(HITId=args.hitid)
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(response)
