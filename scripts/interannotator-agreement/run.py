fe4c4ecd207498a537c16cb78971ec83a2cd44dc
Galen Han <Mastergalen@users.noreply.github.com>
2018-03-15 14:34:29 +0000

    Calculate agreement using Fleiss' kappa

--- scripts/interannotator-agreement/run.py
+++ scripts/interannotator-agreement/run.py
@@ -2,6 +2,8 @@ import argparse
 import numpy as np
 import os
 import json
+import time
+from lib import agreement
 from lib.database.models import *
 from lib.types import relationship_to_id
 from peewee import fn
@@ -13,10 +15,10 @@ args = parser.parse_args()
 
 
 def inter_annotator_score(clip_id):
-    res = VideoClip\
-        .select()\
-        .join(Assignment)\
-        .join(Annotation)\
+    res = VideoClip \
+        .select() \
+        .join(Assignment) \
+        .join(Annotation) \
         .where(VideoClip.id == clip_id)
 
     video_clip = res.get()
@@ -26,6 +28,7 @@ def inter_annotator_score(clip_id):
     all_ids = all_ids_in_clip(clip_id)
     assignment_count = len(vid_dict['assignment_set'])
 
+    # Convert annotations into a matrix
     annotations = np.zeros((assignment_count, len(all_ids), len(all_ids)), dtype=np.uint8)
     for i, assignment in enumerate(vid_dict['assignment_set']):
         for annotation in assignment['annotation_set']:
@@ -34,7 +37,9 @@ def inter_annotator_score(clip_id):
             relationship_id = relationship_to_id[annotation['relationship']]
             annotations[i, start_idx, end_idx] = relationship_id
 
-    pass
+    M = agreement.collapse(annotations, len(relationship_to_id))
+    kappa = agreement.fleiss_kappa(M)
+    print("Video {} Kappa {}".format(clip_id, kappa))
 
 
 def all_ids_in_clip(clip_id):
@@ -43,6 +48,7 @@ def all_ids_in_clip(clip_id):
     :return: Set of IDs in video
     """
     json_path = os.path.join(args.videos_dir, '{}.json'.format(clip_id))
+
     with open(json_path) as f:
         data = json.load(f)
 
@@ -54,17 +60,17 @@ def all_ids_in_clip(clip_id):
     return sorted(ids)
 
 
-query = Assignment\
-    .select(Assignment.video_clip_id, fn.COUNT('*'))\
-    .group_by(Assignment.video_clip_id)\
+query = Assignment \
+    .select(Assignment.video_clip_id, fn.COUNT('*')) \
+    .where(Assignment.assignment_status == 'Approved') \
+    .group_by(Assignment.video_clip_id) \
     .having(fn.COUNT('*') > 1)
 
-print(query.sql())
+video_clips = list(query.dicts())
 
-data = list(query.dicts())
+print("Processing {} videos".format(len(video_clips)))
 
-print(data)
-
-for row in data:
+for row in video_clips:
+    start = time.time()
     inter_annotator_score(row['video_clip_id'])
-
+    print("Took {}s".format(time.time() - start))
