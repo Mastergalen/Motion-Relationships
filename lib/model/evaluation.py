4811c5be0d083d1b7cb898cdc6b8f39ce30d5a0c
Galen Han <Mastergalen@users.noreply.github.com>
2018-04-19 19:46:07 +0100

    Random benchmark

--- lib/model/evaluation.py
+++ lib/model/evaluation.py
@@ -8,7 +8,16 @@ from sklearn.metrics import f1_score
 def evaluate(trained_model, x_test, y_test):
     y_pred = np.argmax(trained_model.predict(x_test), axis=1)
     y_true = np.argmax(y_test, axis=1)
+
+    draw_confusion_matrix(y_true, y_pred)
+
+
+def draw_confusion_matrix(y_true, y_pred):
     cf_matrix = confusion_matrix(y_true, y_pred)
+
+    class_wise_f1 = np.round(f1_score(y_true, y_pred, average=None)*100)*0.01
+    f_score = np.mean(class_wise_f1)
+    print('the mean-f1 score: {:.3f}'.format(f_score))
 
     f, (ax1, ax2) = plt.subplots(1, 2)
     classes = ['No relationship', 'Avoids', 'Gives-way', 'Group']
