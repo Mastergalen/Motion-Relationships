10cce8cb9d8b816f0c6afe3cc7bc1ee8bb16993d
Galen Han <Mastergalen@users.noreply.github.com>
2018-04-19 15:28:06 +0100

    Use cached bottleneck for training in Keras

--- lib/model/evaluation.py
+++ lib/model/evaluation.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import itertools
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 
@@ -6,14 +8,39 @@ from sklearn.metrics import f1_score
 def evaluate(trained_model, x_test, y_test):
     y_pred = np.argmax(trained_model.predict(x_test), axis=1)
     y_true = np.argmax(y_test, axis=1)
-    draw_cf_matrix(y_true, y_pred)
-
-
-def draw_cf_matrix(y_true, y_pred):
     cf_matrix = confusion_matrix(y_true, y_pred)
-    print(cf_matrix)
 
-    class_wise_f1 = np.round(f1_score(y_true, y_pred, average=None)*100)*0.01
-    f_score = np.mean(class_wise_f1)
+    f, (ax1, ax2) = plt.subplots(1, 2)
+    classes = ['No relationship', 'Avoids', 'Gives-way', 'Group']
+    plot_confusion_matrix(cf_matrix, axes=ax1, title='Without normalisation', classes=classes)
+    plot_confusion_matrix(cf_matrix, axes=ax2, title='Normalisation', normalize=True, classes=classes)
 
-    print('the mean-f1 score: {:.2f}'.format(f_score))
+
+def plot_confusion_matrix(cm, classes, axes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.get_cmap("Blues")):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.sca(axes)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        axes.text(j, i, format(cm[i, j], fmt),
+                  horizontalalignment="center",
+                  color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
---
+++ scripts/kinetics/save_i3d_bottleneck.py
@@ -0,0 +1,80 @@
+"""
+Save bottleneck features of kinetics-i3d
+https://github.com/deepmind/kinetics-i3d
+"""
+import argparse
+import numpy as np
+import os
+import glob
+import progressbar
+import tensorflow as tf
+
+from lib.model.architectures import i3d
+from lib.model.config import CONFIG
+from lib.model.loaders.kinetics import KineticsLoader
+
+_BATCH_SIZE = 1
+
+_SAMPLE_VIDEO_FRAMES = 150
+_IMAGE_SIZE = 224
+_NUM_CLASSES = CONFIG['num_relationships']
+
+_DATA_DIR = 'data/kinetics'
+
+_CHECKPOINT_PATHS = {
+    'rgb': 'data/weights/kinetics-i3d/rgb_scratch/model.ckpt',
+    'flow': 'data/weights/kinetics-i3d/flow_scratch/model.ckpt',
+    'rgb_imagenet': 'data/weights/kinetics-i3d/rgb_imagenet/model.ckpt',
+    'flow_imagenet': 'data/weights/kinetics-i3d/flow_imagenet/model.ckpt',
+}
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
+
+def main(unused_argv):
+    # TODO: Train RGB and flow models separately
+
+    rgb_input = tf.placeholder(
+        tf.float32,
+        shape=(_BATCH_SIZE, _SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 3))
+    rgb_y = tf.placeholder(tf.int64, [None, _NUM_CLASSES])
+
+    with tf.variable_scope('RGB'):
+        rgb_model = i3d.InceptionI3d(
+            _NUM_CLASSES, spatial_squeeze=True, final_endpoint='Mixed_5c')
+        rgb_bottleneck, _ = rgb_model(
+            rgb_input, is_training=False, dropout_keep_prob=1.0)
+
+    rgb_variable_map = {}
+    for variable in tf.global_variables():
+        if variable.name.split('/')[0] == 'RGB':
+            rgb_variable_map[variable.name.replace(':0', '')] = variable
+    rgb_saver = tf.train.Saver(var_list=rgb_variable_map, reshape=True)
+
+    dataset = KineticsLoader(_BATCH_SIZE, _NUM_CLASSES, 'training')
+
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        print('Restoring weights...')
+        rgb_saver.restore(sess, _CHECKPOINT_PATHS['rgb'])
+
+        video_data_paths = glob.glob(os.path.join(_DATA_DIR, 'rgb_*.npy'))
+        assert len(video_data_paths) > 0
+
+        bar = progressbar.ProgressBar()
+        for path in bar(video_data_paths):
+            clip_id = '_'.join(os.path.splitext(os.path.basename(path))[0].split('_')[1:])
+            x = np.zeros((1, _SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 3))
+            x[0, ...] = np.load(path)
+            out_bottleneck = sess.run(rgb_bottleneck, feed_dict={rgb_input: x})
+
+            np.save('data/kinetics_bottleneck/{}.npy'.format(clip_id), out_bottleneck)
+
+
+if __name__ == '__main__':
+    tf.app.run(main)
